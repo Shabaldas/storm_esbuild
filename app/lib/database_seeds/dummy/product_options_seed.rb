c74a9013fecb4415df87979ef7845367a218de27
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-02-21 20:21:26 +0200

    (refactoring): product_option_seed

--- app/lib/database_seeds/dummy/product_options_seed.rb
+++ app/lib/database_seeds/dummy/product_options_seed.rb
@@ -11,54 +11,44 @@ module DatabaseSeeds
         # Product_for_parent_category
         Product.first(8).each do |product|
           # seed only colors option
-          color_option.option_values.pluck(:value).each do |option_value|
-            product.product_options.find_or_create_by(option: color_option, primary: true).product_option_values.find_or_create_by!(
-              option_value: color_option.option_values.find_by(value: option_value)
-            )
-          end
+          seed_color_product_options(color_option, product)
         end
 
         Product.where(id: 23..40).each do |product|
           # seed only size option
-          size_option.option_values.pluck(:value).each.with_index(1) do |option_value, index|
-            product.product_options.find_or_create_by(option: size_option).product_option_values.find_or_create_by!(
-              option_value: size_option.option_values.find_by(value: option_value),
-              price: 10.0 * index
-            )
-          end
+          seed_size_product_options(size_option, product)
         end
 
         Product.where(id: 41..54).each do |product|
           # seed colors&size options
-          color_option.option_values.pluck(:value).each do |option_value|
-            product.product_options.find_or_create_by(option: color_option, primary: true).product_option_values.find_or_create_by!(
-              option_value: color_option.option_values.find_by(value: option_value)
-            )
-          end
-
-          size_option.option_values.pluck(:value).each.with_index(1) do |option_value, index|
-            product.product_options.find_or_create_by(option: size_option).product_option_values.find_or_create_by!(
-              option_value: size_option.option_values.find_by(value: option_value),
-              price: 10.0 * index
-            )
-          end
+          seed_color_product_options(color_option, product)
+          seed_size_product_options(size_option, product)
         end
 
         # Product_for_child_category
         Product.last(50).each do |product|
           # seed colors&size options
-          color_option.option_values.pluck(:value).each do |option_value|
-            product.product_options.find_or_create_by(option: color_option, primary: true).product_option_values.find_or_create_by!(
-              option_value: color_option.option_values.find_by(value: option_value)
-            )
-          end
+          seed_color_product_options(color_option, product)
+          seed_size_product_options(size_option, product)
+        end
+      end
 
-          size_option.option_values.pluck(:value).each.with_index(1) do |option_value, index|
-            product.product_options.find_or_create_by(option: size_option).product_option_values.find_or_create_by!(
-              option_value: size_option.option_values.find_by(value: option_value),
-              price: 10.0 * index
-            )
-          end
+      private
+
+      def seed_size_product_options(option, product)
+        option.option_values.pluck(:value).each.with_index(1) do |option_value, index|
+          product.product_options.find_or_create_by(option:).product_option_values.find_or_create_by!(
+            option_value: option.option_values.find_by(value: option_value),
+            price: 10.0 * index
+          )
+        end
+      end
+
+      def seed_color_product_options(option, product)
+        option.option_values.pluck(:value).each do |option_value|
+          product.product_options.find_or_create_by(option:, primary: true).product_option_values.find_or_create_by!(
+            option_value: option.option_values.find_by(value: option_value)
+          )
         end
       end
     end
