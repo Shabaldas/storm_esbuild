b5974bde7b540c7f5a30884125304ebdb258c63e
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-02-27 19:28:55 +0200

    Test Search with ransack

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  include Pagy::Backend
   def index
     @product_categories = ProductCategory.all
-    @one_category = ProductCategory.first
+    # @one_category = ProductCategory.first
+    search_params = params.permit(:format, :page, q: [:name_cont, product_category_id_eq_any: []])
     @parents_product_categories = ProductCategory.where(ancestry: nil)
-    @products = Product.all
+    @q = Product.ransack(search_params[:q])
+    products = @q.result(distinct: true).order(created_at: :asc)
+    @pagy, @products = pagy_countless(products, items: 12)
   end
 
   def show
     @product = Product.find(params[:id])
   end
+
+  private
+  # standard_sizes_id_eq_any_cont
+  # def search_params
+  #   params.permit(:format, :page, q: [:name_cont, :product_category_id_cont])
+  #   params.require(:q).permit!
+  # end
 end
