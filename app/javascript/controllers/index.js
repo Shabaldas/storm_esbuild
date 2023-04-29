fdd7f75dd0c4ee27136d6333d6cd75e9eaeec383
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-04-30 01:28:27 +0300

    add dropzone JS controller to project create in admin

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -25,3 +25,6 @@ application.register("checkbox-family", CheckboxFamilyController)
 import ToggleController from "./toggle_controller"
 application.register("toggle", ToggleController)
 
+import DropzoneController from "./dropzone_controller"
+application.register("dropzone", DropzoneController)
+
