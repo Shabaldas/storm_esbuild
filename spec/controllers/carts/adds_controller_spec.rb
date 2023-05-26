2c29bdba72e8aefcd43a1aea23989de34b83cd86
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-05-26 23:49:04 +0300

    (feat): add missed rspec fileto carts

---
+++ spec/controllers/carts/adds_controller_spec.rb
@@ -0,0 +1,28 @@
+describe Carts::AddsController do
+  describe 'POST /carts/adds' do
+    let(:product) { create(:product) }
+    let(:cart) { create(:cart) }
+
+    before do
+      allow(controller).to receive(:current_cart).and_return(cart)
+    end
+
+    context 'when product exists' do
+      it 'creates a new cart_item' do
+        post :create, params: { product_id: product.id }
+
+        expect(cart.cart_items.count).to eq(1)
+        expect(cart.cart_items.first.quantity).to eq(1)
+        expect(cart.cart_items.first.product).to eq(product)
+      end
+    end
+
+    context 'when product does not exist' do
+      it 'does not create a new cart_item' do
+        post :create, params: { product_id: 0 }
+
+        expect(cart.cart_items.count).to eq(0)
+      end
+    end
+  end
+end
