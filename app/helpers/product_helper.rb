a255f5c209c756da0291f37e8707600d1439d5b1
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-06-08 00:45:31 +0300

    Adaptize prodcut show page with product attributes

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ProductHelper
+
+  def size_option_value_for_select
+    OptionValue.where(option: Option.find_by(measurement: 'mm')).map { |option_value| [option_value.value, option_value.id] }
+  end
 end
