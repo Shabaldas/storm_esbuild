0e21fa97e1e862014e570c367473ab473709fea7
Volodymyr <33484674+Shabaldas@users.noreply.github.com>
2023-06-08 00:57:20 +0300

    (fix): rubocop issues

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module ProductHelper
-
   def size_option_value_for_select
     OptionValue.where(option: Option.find_by(measurement: 'mm')).map { |option_value| [option_value.value, option_value.id] }
   end
