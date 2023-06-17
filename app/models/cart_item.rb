fde3187538d89dc33a57c82dc8f553e8ad245861
Volodymyr <shabaldasengr@gmail.com>
2023-06-18 01:20:37 +0300

    Add after create brodcast in cart model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -9,6 +9,28 @@ class CartItem < ApplicationRecord
   accepts_nested_attributes_for :cart_item_option_values
 
   # TODO after create commit
+  after_create_commit do
+    broadcast_replace_to cart,
+                         target: 'cart_body',
+                         partial: 'cart/empty_cart_body',
+                         locals: { current_cart: cart }
+
+    broadcast_append_to cart,
+                        target: 'cart_item',
+                        partial: 'cart/item_line',
+                        locals: { cart_item: self }
+ 
+    broadcast_replace_to cart,
+                         target: 'footer_action',
+                         partial: 'cart/cart_footer_action',
+                         locals: { current_cart: cart }
+
+    broadcast_replace_to cart,
+                         target: 'total_price',
+                         partial: 'cart/total_price',
+                         locals: { current_cart: cart }
+  end
+
   after_update_commit do
     broadcast_replace_to cart,
                          target: "quantity_cart_item_#{id}",
