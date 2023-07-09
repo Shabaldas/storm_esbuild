8b109c475f81596e925eb02d49686eac38e4ff05
vita888-ui <vityska14@gmail.com>
2023-07-09 14:36:46 +0300

    Storm_F[11]:fix product filters

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       md: "768px",
       lg: "1025px",
       xl: "1280px",
-      "2xl": "1536px",
+      "2xl": "1440px",
     },
     colors: {
       transparent: "transparent",
