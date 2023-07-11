89f0a91c91ee3ae6e292ad2ff3e0db0ad8086429
Volodymyr <shabaldasengr@gmail.com>
2023-07-11 13:42:57 +0300

    STORM_B (feat): add google analitics to website

---
+++ app/helpers/google_analytics_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module GoogleAnalyticsHelper
+  def google_analytics_head_script
+    return unless Rails.env.production?
+
+    javascript_tag nonce: true do
+      <<-JAVASCRIPT.html_safe
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','G-QMC1QVF8BG');
+      JAVASCRIPT
+    end
+  end
+end
