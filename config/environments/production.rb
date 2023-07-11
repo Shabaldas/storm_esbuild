3c0ca14c9981f67045783cd5d13e502351057e1b
Volodymyr <shabaldasengr@gmail.com>
2023-07-11 23:59:12 +0300

    STORM_B (feat): update email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {
-    host: '',
+    host: '3dstorm.com.ua',
     protocol: 'https'
   }
 
