c036fe0931960cc4c37fcfe3ec4599db30440ce3
Volodymyr <shabaldasengr@gmail.com>
2023-07-27 20:18:40 +0300

    Finish pricing page with selectors

--- app/javascript/controllers/selected_material_and_price_controller.js
+++ app/javascript/controllers/selected_material_and_price_controller.js
@@ -14,10 +14,26 @@ export default class extends Controller {
   }
 
   changeSla(event) {
-    
+    const frame25 = document.getElementById('printing_sla_25');
+    const frame35 = document.getElementById('printing_sla_35');
+    const frame50 = document.getElementById('printing_sla_50');
+    frame25.src=event.target.value;
+    frame35.src=event.target.value;
+    frame50.src=event.target.value;
+    frame25.reload();
+    frame35.reload();
+    frame50.reload();
   }
+  
   changeDlp(event) {
-
+    const frame25 = document.getElementById('printing_dlp_25');
+    const frame35 = document.getElementById('printing_dlp_35');
+    const frame50 = document.getElementById('printing_dlp_50');
+    frame25.src=event.target.value;
+    frame35.src=event.target.value;
+    frame50.src=event.target.value;
+    frame25.reload();
+    frame35.reload();
+    frame50.reload();
   }
-
 }
