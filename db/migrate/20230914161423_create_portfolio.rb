ff68200d9231e920472d8d99c793fb18d434daf9
Volodymyr <shabaldasengr@gmail.com>
2023-09-14 20:03:04 +0300

    STORM_B (feat): add Portfolio model

---
+++ db/migrate/20230914161423_create_portfolio.rb
@@ -0,0 +1,15 @@
+class CreatePortfolio < ActiveRecord::Migration[7.0]
+  def change
+    create_table :portfolios do |t|
+      t.string :name
+      t.string :category_name
+      t.string :description
+      t.integer :status, default: 0
+      t.integer :portfolio_type, default: 0
+      t.string :tags
+      t.string :created_by_machine
+
+      t.timestamps
+    end
+  end
+end
