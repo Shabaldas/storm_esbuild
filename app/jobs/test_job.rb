ec11546724ffbaaa8d5dec7e1c8a28bd93916944
Volodymyr <shabaldasengr@gmail.com>
2023-09-26 20:22:26 +0300

    (fix): linter check

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/Output
 class TestJob
   include Sidekiq::Worker
 
   def perform
-    puts 'I am , running my first job at' # rubucop:disable Rails/Output
+    puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
   end
 end
+# rubocop:enable Rails/Output
