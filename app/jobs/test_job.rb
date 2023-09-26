31da07b872cd613926e37b7e2e519f74eff7aaab
Volodymyr <shabaldasengr@gmail.com>
2023-09-26 22:27:54 +0300

    (fix): add test job

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/Output
 class TestJob
   include Sidekiq::Worker
 
   def perform
+    puts '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
     puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts 'I am , running my first job at'
+    puts '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
   end
 end
+# rubocop:enable Rails/Output
