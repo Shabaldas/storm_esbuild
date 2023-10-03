f2a09b7571fecc8e12957690e1c95bd2f1565ffa
Volodymyr <shabaldasengr@gmail.com>
2023-10-03 14:32:09 +0300

    (feat): finish cost page for admin

--- app/controllers/dredd/costs_controller.rb
+++ app/controllers/dredd/costs_controller.rb
@@ -2,21 +2,32 @@
 
 module Dredd
   class CostsController < BaseController
+    before_action :set_cost, only: :destroy
+
     def create
       @cost = Cost.new(cost_params)
       @cost.user = current_user
 
       if @cost.save
-        redirect_to monthly_dredd_accountings_path(date: params[:cost][:month_date]), info: 'Cost created'
+        redirect_to monthly_dredd_accountings_path(date: params[:cost][:month_date]), info: { text: 'Cost created', icon: 'success_icon' }
       else
-        redirect_to monthly_dredd_accountings_path(date: params[:cost][:month_date]), error: 'Cost not created'
+        redirect_to monthly_dredd_accountings_path(date: params[:cost][:month_date]), error: { text: 'Cost not created', icon: 'attention' }
       end
+    end
+
+    def destroy
+      @cost.destroy!
+      redirect_to monthly_dredd_accountings_path(date: params[:month_date]), info: { text: 'Cost deleted', icon: 'success_icon' }
     end
 
     private
 
     def cost_params
       params.require(:cost).permit(:name, :user, :description, :amount, :date)
+    end
+
+    def set_cost
+      @cost = Cost.find(params[:id])
     end
   end
 end
