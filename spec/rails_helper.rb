990c9fd60da1ffe77f803532303e051a2245d897
Volodymyr <shabaldasengr@gmail.com>
2023-10-11 12:05:55 +0300

    (fix): configure CI tasks

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -101,9 +101,31 @@ RSpec.configure do |config|
 
   WebMock.allow_net_connect!(net_http_connect_on_start: true)
 
-  Capybara.register_driver :firefox do |mode|
-    Capybara::Selenium::Driver.new mode, browser: :remote, desired_capabilities: :firefox
+  # Capybara.register_driver :firefox do |mode|
+  #   Capybara::Selenium::Driver.new mode, browser: :remote, desired_capabilities: :firefox
+  # end
+
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    version = Capybara::Selenium::Driver.load_selenium
+    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.add_argument("--headless")
+      opts.add_argument("--disable-gpu") if Gem.win_platform?
+      # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+      opts.add_argument("--disable-site-isolation-trials")
+      opts.add_preference("download.default_directory", Capybara.save_path)
+      opts.add_preference(:download, default_directory: Capybara.save_path)
+    end
+  
+    Capybara::Selenium::Driver.new(app, **{ browser: :chrome, options_key => browser_options })
   end
+  
+  RSpec.configure do |config|
+    config.before(:each, type: :system) do
+      driven_by :selenium_chrome_headless
+    end
+  end
+  
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
