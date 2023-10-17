54b20d871837fbe4a946626c52efd15f533d92bc
Volodymyr <shabaldasengr@gmail.com>
2023-10-17 20:42:49 +0300

    STORM_B (feat): add new status to manual orders

--- app/helpers/dredd/manual_order_helper.rb
+++ app/helpers/dredd/manual_order_helper.rb
@@ -18,5 +18,20 @@ module Dredd
         'google_icon'
       end
     end
+
+    def workflow_status_icon(workflow_status)
+      case workflow_status.to_sym
+      when :nothing
+        'empty_basket'
+      when :modeling
+        '3d_modeling_icon'
+      when :printing
+        '3d_printing_icon'
+      when :call_cleint
+        'phone_icon'
+      else
+        'success'
+      end
+    end
   end
 end
