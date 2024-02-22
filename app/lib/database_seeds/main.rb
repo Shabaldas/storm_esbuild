19c50fcd550e68f90929a292df755432b5f75741
andrii.bukhalo <andrii.bukhalo@cd.p2h.com>
2024-02-22 10:44:54 +0200

    minor fix

--- app/lib/database_seeds/main.rb
+++ app/lib/database_seeds/main.rb
@@ -13,16 +13,16 @@ module DatabaseSeeds
       DatabaseSeeds::Dummy::ProductsSeed.new.execute
 
       # Real
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_frankivsk_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_lviv_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_ternopil_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_chernivtsi_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_rivne_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_volyn_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_zakarpattia_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_khmelnytskyi_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_vinnytsia_region_cities
-      # DatabaseSeeds::Real::CitiesSeed.new.execute_kyiv_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_frankivsk_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_lviv_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_ternopil_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_chernivtsi_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_rivne_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_volyn_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_zakarpattia_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_khmelnytskyi_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_vinnytsia_region_cities
+      DatabaseSeeds::Real::CitiesSeed.new.execute_kyiv_region_cities
     end
   end
 end
