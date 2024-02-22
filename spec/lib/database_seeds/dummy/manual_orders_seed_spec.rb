471992b2d2cc8dc86e635cf3d52546f89ce5b3cf
andrii.bukhalo <andrii.bukhalo@cd.p2h.com>
2024-02-22 10:26:49 +0200

    STORM_B: add new table client and add rilation

--- spec/lib/database_seeds/dummy/manual_orders_seed_spec.rb
+++ spec/lib/database_seeds/dummy/manual_orders_seed_spec.rb
@@ -1,11 +1,25 @@
 RSpec.describe DatabaseSeeds::Dummy::ManualOrdersSeed do
   describe '#execute' do
-    context 'when executing seed' do
-      before { described_class.new.execute }
+    let(:client) { create(:client) }
 
-      it 'creates 25 manual orders' do
-        expect(ManualOrder.count).to eq(25)
-      end
+    before do
+      allow(Client).to receive(:all).and_return([client])
+      allow(FactoryBot).to receive(:create)
     end
+
+    # rubocop:disable RSpec/MessageSpies
+    it 'creates manual orders for each client' do
+      expect(FactoryBot).to receive(:create).exactly(5).times.with(
+        :manual_order,
+        first_name: client.first_name,
+        last_name: client.last_name,
+        email: client.email,
+        phone_number: client.phone_number,
+        client_id: client.id
+      )
+
+      described_class.new.execute
+    end
+    # rubocop:enable RSpec/MessageSpies
   end
 end
