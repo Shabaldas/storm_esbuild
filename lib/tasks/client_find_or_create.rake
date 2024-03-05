172f50f9cd48dba47e249f1bc6d21d50ec7a6a3c
andrii.bukhalo <andrii.bukhalo@cd.p2h.com>
2024-03-05 17:45:16 +0200

    add new ability for check code in phone number

--- lib/tasks/client_find_or_create.rake
+++ lib/tasks/client_find_or_create.rake
@@ -4,7 +4,7 @@ namespace :client_find_or_create do
   desc 'Find or create Client from Manual Orders'
   task manual_order: :environment do
     ManualOrder.find_each do |manual_order|
-      client = Client.find_or_initialize_by(phone_number: manual_order.phone_number, first_name: manual_order.first_name,
+      client = Client.find_or_initialize_by(phone_number: manual_order.phone_number[-10..], first_name: manual_order.first_name,
                                             last_name: manual_order.last_name) do |new_client|
         new_client.first_name = manual_order.first_name
         new_client.last_name = manual_order.last_name
--- spec/task/client_find_or_create_spec.rb
+++ spec/task/client_find_or_create_spec.rb
@@ -1,11 +1,8 @@
 # rubocop:disable RSpec/DescribeClass
-RSpec.describe 'client_find_or_create Rake Task' do
-  before do
-    Rake.application.load_rakefile
-    Rake::Task.define_task(:environment)
-  end
+Rails.application.load_tasks
 
-  describe 'Create new client' do
+RSpec.describe 'client_find_or_create Rake Task' do
+  describe 'create new client' do
     let(:worker) { create(:worker) }
     let!(:manual_order) { create(:manual_order, worker_id: worker.id) }
     let!(:manual_orders) { create_list(:manual_order, 5, worker_id: worker.id) }
@@ -20,19 +17,35 @@ RSpec.describe 'client_find_or_create Rake Task' do
     end
   end
 
-  describe 'Client whas exist' do
-    let!(:worker) { create(:worker) }
-    let!(:manual_order) { create(:manual_order, first_name: 'John', last_name: 'Doe', phone_number: '380951130020') }
-    let!(:client) { create(:client, first_name: 'John', last_name: 'Doe', phone_number: '380951130020') }
+  describe 'client whas exist' do
+    context 'when not create new client when in data from Manual is exist in client' do
+      let!(:worker) { create(:worker) }
+      let!(:manual_order) { create(:manual_order, first_name: 'John', last_name: 'Doe', phone_number: '380951130020') }
+      let!(:client) { create(:client, first_name: 'John', last_name: 'Doe', phone_number: '380951130020') }
 
-    it 'chech not create new client if find client in DB' do
-      Rake::Task['client_find_or_create:manual_order'].invoke
+      it 'chech not create new client if find client in DB' do
+        Rake::Task['client_find_or_create:manual_order'].invoke
 
-      expect(Client.count).to eq(1)
-      expect(manual_order.reload.client_id).to eq(manual_order.client_id)
-      expect(manual_order.reload.worker_id).to eq(manual_order.client_id)
-      expect(manual_order.reload.worker_id).not_to eq(worker.id)
-      expect(client.attributes).to eq(client.reload.attributes)
+        expect(Client.count).to eq(1)
+        expect(manual_order.reload.client_id).to eq(manual_order.client_id)
+        expect(manual_order.reload.worker_id).to eq(manual_order.client_id)
+        expect(manual_order.reload.worker_id).not_to eq(worker.id)
+        expect(client.attributes).to eq(client.reload.attributes)
+      end
+    end
+
+    context 'when phone is with code and order is without code' do
+      let!(:worker) { create(:worker) }
+      let!(:manual_order) { create(:manual_order, first_name: 'John', last_name: 'Doe', phone_number: '0951130020') }
+      let!(:client) { create(:client, first_name: 'John', last_name: 'Doe', phone_number: '380951130020') }
+
+      it 'not create new client in DB' do
+        Rake::Task['client_find_or_create:manual_order'].invoke
+
+        expect(Client.count).to eq(1)
+        expect(manual_order.reload.worker_id).not_to eq(worker.id)
+        expect(client.attributes).to eq(client.reload.attributes)
+      end
     end
   end
 end
